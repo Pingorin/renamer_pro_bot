f809176ab40748e4d6102f353c3906aefff59c0c
Pingorin <125970228+Pingorin@users.noreply.github.com>
2023-05-04 17:18:51 +0530

    Add files via upload

---
+++ helper/txt.py
@@ -0,0 +1,81 @@
+"""
+Apache License 2.0
+
+Copyright (c) 2022 @LazyDeveloper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+Telegram Link : https://t.me/LazyDeveloper 
+Repo Link : https://github.com/LazyDeveloperr/Gangster-Baby-Renamer-BOT
+License Link : https://github.com/LazyDeveloperr/Gangster-Baby-Renamer-BOT/blob/main/LICENSE
+"""
+
+
+class mr(object):
+    PROGRESS_BAR = """\n
+╭━━━━❰ Gangster Hacking... ❱━➣
+┣⪼ 🗂️ : {1} | {2}
+┣⪼ ⏳️ : {0}%
+┣⪼ 🚀 : {3}/s
+┣⪼ ⏱️ : {4}
+╰━━━━━━━━━━━━━━━➣ """
+
+    ABOUT_TXT = """
+╭───────────⍟
+├🤖 The Name is : {}
+├👑 ADMIN-TG : <a href=https://t.me/LazyDeveloper>LazyDeveloper</a> 
+├👑 Instagram : <a href=https://www.instagram.com/LazyDeveloperrr>C-Insta</a> 
+├📕 LIBRARY : <a href=https://github.com/pyrogram>Pyrogram</a>
+├✏️ LANGUAGE : <a href=https://www.python.org>Python 3</a>
+├💾 DATABASE : <a href=https://cloud.mongodb.com>MongoDB</a>
+├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁1 : <a href=https://apps.koyeb.com>Koyeb</a>
+├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁2 : <a href=https://dashboard.heroku.com>Heroku</a>
+├📊 BUILD-STATUS : Python v3.6.8               
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+🌌 <b><u>HOW TO SET THUMBNILE</u></b>
+  
+•> /start a bot and send any picture to automatically set thumbnile.
+•> /delthumb use this command and delete your old thumbnile.
+•> /viewthumb use this command view your current thumbnile.
+
+📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
+•> /set_caption - set a custom caption
+•> /see_caption - see your custom caption
+•> /del_caption - delete custom caption
+
+Example:- /set_caption 📕 File Name: {filename}
+💾 Size: {filesize}
+⏰ Duration: {duration}
+
+✏️ <b><u>HOW TO RENAME A FILE</u></b>
+•> send any file and click rename option and type new file name and \n send select [ document, video, audio ]👈 choice this.
+ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://www.instagram.com/LazyDeveloperrr>Direct Message</a>
+"""
+
+#⚠️ don't remove our credits 🙏😢😢
+    DEV_TXT = """
+<b><u>Special Thanks & Developers</b></u> 
+
+» 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 : <a href=https://www.youtube.com/channel/UCY-iDra0x2hdd9PdHKcZkRw>Watch Tutorial</a>
+
+• 😈 <a href=https://github.com/LazyDeveloperr>LazyDeveloper Github</a>
+• 😈 <a href=https://t.me/LazyDeveloper>LazyDeveloper Tg Channel</a>
+• 😈 <a href=https://t.me/mRiderDM>Direct-Msg-TG</a>
+• 😈 <a href=https://t.me/real_MoviesAdda1>Join Movies Adda</a>
+• 😈 <a href=https://t.me/GitHub_noob>Thanks- Github Noob</a>
+"""
